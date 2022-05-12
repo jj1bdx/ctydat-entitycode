38cc6d6712b75d6f83b2b42c11bb25d6ac9512f0
Kenji Rikitake <kenji.rikitake@acm.org>
2022-05-12 12:08:15 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# ctydat-entitycode
+
+This repository contains conversion key-value table implementations betweem CTY.DAT Main Prefixes and ARRL DXCC Entity Codes.
+
+All contents are manually verified by the author.
+
+## Author
+
+Kenji Rikitake, JJ1BDX
+
+## License
+
+MIT
+
---
+++ generation-script.sh
@@ -0,0 +1,4 @@
+!#/bin/sh
+awk '{print $1 "," $2;}' prefix_to_entitycode.txt > prefix_to_entitycode.csv
+awk '{print $2 "," $1;}' prefix_to_entitycode.txt | sort -n -t , > entitycode_to_prefix.csv
+awk '{print $2 "  " $1;}' prefix_to_entitycode.txt | sort -n > entitycode_to_prefix.txt
